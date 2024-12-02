2f1ac1c1206d8143710fe9c4687b7efc6bbe8250
edwardtimsack <edwardtimothysackie@gmail.com>
2024-12-02 10:23:40 +0000

    completed the about_me page

---
+++ scripts/aboutMe.js
@@ -0,0 +1,57 @@
+const appName = document.querySelector('#me');
+const menu = document.querySelector('#ham');
+const cancelMenu = document.querySelector('#close')
+const searchIcon = document.querySelector('#searching');
+const header = document.querySelector('header')
+const main = document.querySelector('.main');
+const more = document.querySelector('.more');
+const lastestProjects = document.querySelector('.lastest_projects');
+const skillsSet = document.querySelector('.skills_set');
+const footer = document.querySelector('footer');
+const copyTerms = document.querySelector('.copy_terms');
+const mobile = document.querySelector('.mobile');
+const desktopView = document.querySelector('.desktop_view');
+
+const mobileView = document.querySelector('.mobile_view');
+
+// console.log(menu);
+
+
+menu.addEventListener('click', displayNav);
+cancelMenu.addEventListener('click', hideNav);
+
+
+function displayNav(){
+    // menu.style.fontSize = '5rem';
+    console.log('helloooooo')
+    mobileView.style.display = 'flex';
+    // desktopView.style.display = 'none';
+    appName.style.display = 'none';
+    searchIcon.style.display = 'none';
+    cancelMenu.style.display = 'block';
+    ham.style.display = 'none';
+    desktopView.style.display = 'none';
+    main.style.display = 'none';
+    more.style.display = 'none';
+    lastestProjects.style.display = 'none';
+    skillsSet.style.display = 'none';
+    footer.style.display = 'none';
+    copyTerms.style.display = 'none';
+
+}
+
+
+function hideNav() {
+    console.log('hiiiii')
+    appName.style.display = 'block'
+    searchIcon.style.display = 'block'
+    cancelMenu.style.display = 'none';
+    ham.style.display = 'block';
+    main.style.display = 'flex';
+    more.style.display = 'flex';
+    more.style.flexDirection = 'column';
+    lastestProjects.style.display = 'flex';
+    skillsSet.style.display = 'flex';
+    footer.style.display = 'flex';
+    copyTerms.style.display = 'flex';
+}
